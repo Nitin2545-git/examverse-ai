c45136d3dbf53156e02300e036c01f0cfa5bf600
Nitin2545-git <888777666aakash@gmail.com>
2026-07-27 10:45:10 +0530

    Add Hero to homepage
    Removed main content and replaced it with Hero component.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,11 @@
 import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-
-      <main>
-        <h1>ExamVerse AI</h1>
-
-        <p>
-          Welcome to India's AI Powered Government Exam Learning Platform.
-        </p>
-
-        <button>Start Learning</button>
-      </main>
+      <Hero />
     </>
   );
 }
