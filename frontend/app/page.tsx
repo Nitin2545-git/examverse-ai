af2ffa209ffebf7ff75af39b5b8a645cf1bc3f2e
Nitin2545-git <888777666aakash@gmail.com>
2026-07-27 10:32:26 +0530

    Add homepage

---
+++ frontend/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>ExamVerse AI</h1>
+
+      <p>
+        Welcome to India's AI Powered Government Exam Learning Platform.
+      </p>
+
+      <button>Start Learning</button>
+    </main>
+  );
+}
